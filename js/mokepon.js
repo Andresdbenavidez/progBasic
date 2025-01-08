801674af2979666bac6b7f0447835683fc66f973
Andres Benavidez <andresdavidbenavidez@gmail.com>
2025-01-08 10:03:23 -0500

    Cambia la logica de seleccion del ataque enemigo.

--- js/mokepon.js
+++ js/mokepon.js
@@ -25,15 +25,16 @@ const contenedorAtaques = document.getElementById('contenedorAtaques')
 let mokepones = []
 let botones = []
 let ataqueJugador = []
+let ataqueEnemigo = []
 
 /*Variables*/
-let ataqueEnemigo
 let opcionDeMokepones
 let inputHipodoge
 let inputCapipepo
 let inputRatigueya
 let mascotaJugador
 let ataquesMokepon
+let ataquesMokeponEnemigo
 let botonFuego
 let botonAgua
 let botonTierra
@@ -185,6 +186,7 @@ function secuenciaAtaque(){
                 console.log(ataqueJugador)
                 boton.style.background = '#112f58'        
             }
+            ataqueAleatorioEnemigo()
         })
     })
 }
@@ -196,19 +198,22 @@ function seleccionarMascotaEnemigo() {
     /*Selecciona un mokepon adentro del array mokepones usando el número aleatorio en el indice. */
     /**Cuando Tengamos el valor imprimelo adentro de esta etiquet(Asi entendi el innerHTML) */
     spanMascotaEnemigo.innerHTML = mokepones[mascotaAleatoria].nombre
+    ataquesMokeponEnemigo = mokepones[mascotaAleatoria].ataques
     secuenciaAtaque()
 }
 
 function ataqueAleatorioEnemigo() {
-    let ataqueAleatorio = aleatorio(1,3)
+    let ataqueAleatorio = aleatorio(0,ataquesMokeponEnemigo.length-1)
     
-    if (ataqueAleatorio == 1) {
-        ataqueEnemigo = 'FUEGO'
-    } else if (ataqueAleatorio == 2) {
-        ataqueEnemigo = 'AGUA'
+    if (ataqueAleatorio == 0 || ataqueAleatorio == 1) {
+        ataqueEnemigo.push('FUEGO')
+    } else if (ataqueAleatorio == 3 || ataqueAleatorio == 4) {
+        ataqueEnemigo.push('AGUA')
     } else {
-        ataqueEnemigo = 'TIERRA'
+        ataqueEnemigo.push('TIERRA')
     }
+
+    console.log(ataqueEnemigo)
 
     combate()
 }
